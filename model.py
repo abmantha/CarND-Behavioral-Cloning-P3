47fae862d87b74f9293b781b21b05975315f5880
abmantha <abhishek.mantha@gmail.com>
2017-08-22 13:18:03 -0700

    Testing the first run on autonomous mode with sample data -- NVIDIA model

--- model.py
+++ model.py
@@ -1,16 +1,17 @@
 import csv
+import cv2
 import numpy as np
 import keras
 from keras.models import Sequential
-from keras.layers import Flat, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda
 from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 
 lines = []
-with open('./data/driving_log.csv', 'rb') as csvfile: 
+with open('../data/driving_log.csv') as csvfile: 
     reader = csv.reader(csvfile)
+    header = next(reader, None)
     for line in reader: 
-        print (line)
         lines.append(line)
 
 images = []
@@ -20,15 +21,14 @@ for line in lines:
         source_path = line[i]
         tokens = source_path.split('/')
         filename = tokens[-1]
-        local_path = "../data/IMG" + filename
-        image = csv2.imread(local_path)
+        local_path = "../data/IMG/" + filename
+        image = cv2.imread(local_path)
         images.append(image)
-    correction = 0.2
-    meaurement = float(line[3])
+        correction = 0.2
+    measurement = float(line[3])
     measurements.append(measurement)
     measurements.append(measurement + correction)
     measurements.append(measurement - correction)        
-    print (local_path)
 
 augmented_images = []
 augmented_measurements = []
@@ -47,7 +47,7 @@ y_train = np.array(measurements)
 # Regression Model
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-model.add(Cropping2D(cropping=(70, 25), (0,0)))
+model.add(Cropping2D(cropping=((70, 25), (0,0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
@@ -60,6 +60,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(optimizer='adam', loss='mse')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epochs=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 
 model.save('model.h5')
