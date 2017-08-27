ce76c4e28e49c8f04015b8c9011d56a775e22a18
abmantha <abhishek.mantha@gmail.com>
2017-08-27 13:38:45 -0700

    Not the best performance with using all 3 cameras and augmentation

--- model.py
+++ model.py
@@ -11,13 +11,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 import keras
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Dropout
 from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 import matplotlib.pyplot as plt
 
 samples = []
-with open('../data-3/driving_log.csv') as csvfile: 
+with open('../data-4/driving_log.csv') as csvfile: 
     reader = csv.reader(csvfile)
     header = next(reader, None)
     for line in reader: 
@@ -65,11 +65,19 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples: 
-                name = '../data-3/IMG/' + batch_sample[0].split('/')[-1]
-                center_image = cv2.imread(name)
+                for i in range(3): 
+                    source_path = batch_sample[i]
+                    tokens = source_path.split('/')
+                    filename = tokens[-1]
+                    name = '../data-4/IMG/' + filename
+                    image = cv2.imread(name)
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+                    images.append(image)
+                correction = 0.
                 center_angle = float(batch_sample[3])
-                images.append(center_image)
                 angles.append(center_angle)
+                angles.append(center_angle + correction)
+                angles.append(center_angle - correction)
 
             augmented_images = []
             augmented_angles = []
@@ -93,15 +101,22 @@ model = Sequential()
 model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160, 320, 3), output_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0,0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Dropout(0.9))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Dropout(0.8))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Dropout(0.7))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Dropout(0.6))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Flatten())
-# Consider adding a dropout layer similar to the traffic sign classification project
 model.add(Dense(100))
+model.add(Dropout(0.5))
 model.add(Dense(50))
+model.add(Dropout(0.5))
 model.add(Dense(10))
+model.add(Dropout(0.5))
 model.add(Dense(1))
 
 model.compile(optimizer='adam', loss='mse')
@@ -119,4 +134,4 @@ history_object = model.fit_generator(train_generator, samples_per_epoch=len(trai
 # plt.legend(['training set', 'validation set'], loc='upper right')
 # plt.show()
 
-model.save('model-5.h5')
+model.save('model-7.h5')
